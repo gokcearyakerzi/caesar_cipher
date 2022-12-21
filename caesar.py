cf01a4b2b8fc9851cb3742dcd0b7447016239f46
gokcearyakerzi <gokcearyakerzi01@gmail.com>
2022-12-21 21:19:04 +0300

    first commit

---
+++ caesar.py
@@ -0,0 +1,56 @@
+logo = """           
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+            88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88           
+"""
+
+should = True
+
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+print(logo)
+
+def caesar(text,shift, direction):
+  final = ""
+  if direction == "decode":
+      shift = shift * -1
+  for char in text:
+    if char in alphabet:
+        position = alphabet.index(char)
+        new_position = position + shift
+        new_letter = alphabet[new_position]
+        final += new_letter
+    else:
+        final += char
+  print(f"The {direction} text is {final}")
+
+while should == True:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    shift = shift % 26
+    caesar(text,shift, direction)
+    answer = input("Type 'yes' if you want to go again. Otherwise type 'no' : ")
+
+    if answer == "no":
+        should = False
+        print("Good Byeeeee!")
+        
+
+from art import logo
+print(logo)
+
+
